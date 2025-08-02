d7d355af6ae53b8c859f9e1f34dcbbf0e67e82c4
Dilsher10 <57361968+Dilsher10@users.noreply.github.com>
2025-08-03 04:52:24 +0500

    changes

--- 05_Components/JavaScript/Autocomplete Search/app.js
+++ 05_Components/JavaScript/Autocomplete Search/app.js
@@ -1,60 +1,115 @@
-const input = document.getElementById('autocomplete-input');
-const resultsBox = document.getElementById('autocomplete-results');
+const input = document.getElementById('input');
+const results = document.getElementById('results');
 let debounceTimer = null;
+let cache = {};
+let currentIndex = -1;
 
-const API_URL = 'https://dummyjson.com/products/search?q=';
+
+
+function handleKeydown(e) {
+    const items = results.querySelectorAll('p');
+
+    switch (e.key) {
+        case 'ArrowDown':
+            e.preventDefault();
+
+            currentIndex = currentIndex + 1;
+            if (currentIndex >= items.length) {
+                currentIndex = 0;
+            }
+            highlightItem(items);
+            input.value = items[currentIndex].textContent;
+            break;
+
+        case 'ArrowUp':
+            e.preventDefault();
+            currentIndex = currentIndex - 1;
+            if (currentIndex < 0) {
+                currentIndex = items.length - 1;
+            }
+            highlightItem(items);
+            input.value = items[currentIndex].textContent;
+            break;
+    }
+}
+
+
+function highlightItem(items) {
+    items.forEach((item, index) => {
+        if (index === currentIndex) {
+            item.classList.add('active');
+            item.scrollIntoView({ block: 'nearest' });
+        } else {
+            item.classList.remove('active');
+        }
+    })
+}
 
 
 
-input.addEventListener('input', () => {
-  const query = input.value.trim();
-
-  clearTimeout(debounceTimer);
-
-  if (query === '') {
-    clearResults();
-    return;
-  }
-
-  debounceTimer = setTimeout(() => {
+function handleFocus() {
+    let query = input.value.trim();
+    if (cache[query]) {
+        renderSuggestion(cache[query])
+    }
     fetchSuggestions(query);
-  }, 300);
-});
+}
 
+
+
+function handleBlur() {
+    setTimeout(() => {
+        clearResults();
+    }, 300)
+}
+
+
+
+function handleInput() {
+    let query = input.value.trim();
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(() => {
+        if (cache[query]) {
+            renderSuggestion(cache[query])
+        }
+        fetchSuggestions(query);
+    }, 300)
+}
 
 
 async function fetchSuggestions(query) {
-  try {
-    const res = await fetch(API_URL + query);
-    const data = await res.json();
-    const products = data.products.slice(0, 10); // show top 10
-    renderResults(products);
-  } catch (error) {
-    clearResults();
-    console.error('Error fetching suggestions:', error);
-  }
+    try {
+        const res = await fetch(`https://dummyjson.com/products/search?q=${query}`);
+        const data = await res.json();
+        const products = data.products.slice(0, 10);
+        cache[query] = products;
+        renderSuggestion(products);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 
-
-function renderResults(items) {
-  clearResults();
-
-  if (items.length === 0) return;
-
-  items.forEach(item => {
-    const div = document.createElement('div');
-    div.textContent = item.title;
-    div.onclick = () => {
-      input.value = item.title;
-      clearResults();
-    };
-    resultsBox.appendChild(div);
-  });
+function renderSuggestion(items) {
+    clearResults()
+    items.forEach((product) => {
+        const p = document.createElement('p');
+        p.textContent = product.title;
+        p.addEventListener('click', () => {
+            input.value = product.title;
+        });
+        results.appendChild(p)
+    });
 }
-
 
 
 function clearResults() {
-  resultsBox.innerHTML = '';
+    results.innerHTML = '';
 }
+
+input.addEventListener('input', handleInput);
+input.addEventListener('focus', handleFocus);
+input.addEventListener('blur', handleBlur);
+input.addEventListener('keydown', handleKeydown);
+
+
