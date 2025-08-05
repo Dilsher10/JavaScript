f49c300f3981a71c07e6bdcba25254407909db96
dilsher10 <dilsher.dahri10@gmail.com>
2025-08-05 20:27:39 +0500

    changes

--- 05_Components/Product Filter/js/main.js
+++ 05_Components/Product Filter/js/main.js
@@ -19,11 +19,7 @@ const priceEl = document.getElementById("filter-price");
 const searchEl = document.getElementById("filter-search");
 
 
-// Filter + render
-function updateAndRender() {
-  const result = filterProducts(products, state.filters);
-  renderProducts(result, productList);
-}
+
 
 // Setup filters
 function setupFilters() {
